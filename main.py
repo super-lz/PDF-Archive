aefb6e50cfa297cff31cd89615abd40eac8d5d42
super-lz <1151228268@qq.com>
2023-07-03 03:08:49 +0800

    fix init_folder

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from src.lib.data_process import process_data
 from src.lib.ui import create_ui
+from src.lib.folder_init import init_folder
 
 
 if __name__ =="__main__":
+    init_folder()
+
     process_data()
 
     create_ui()
