cb4683170eb3758663f38047a10153e537fe2c39
super-lz <1151228268@qq.com>
2023-07-03 09:14:17 +0800

    fix indent

--- src/lib/folder_init.py
+++ src/lib/folder_init.py
@@ -5,14 +5,13 @@ from src.common.util import input_path, output_path, data_path
 
 def init_folder():
     # 检查文件夹是否存在，如果不存在则创建
-	if not os.path.exists(input_path):
-		os.makedirs(input_path)
+    if not os.path.exists(input_path):
+        os.makedirs(input_path)
 
-	if not os.path.exists(output_path):
-		os.makedirs(output_path)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
 
-
-	# 检查data.json文件是否存在，如果不存在则创建空的json文件
-	if not os.path.exists(data_path):
-		with open(data_path, "w") as f:
-			json.dump({}, f)
+    # 检查data.json文件是否存在，如果不存在则创建空的json文件
+    if not os.path.exists(data_path):
+        with open(data_path, "w") as f:
+            json.dump({}, f)
