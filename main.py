d3afd4824d384954271baa0834bc445e65d0ed8b
super-lz <1151228268@qq.com>
2023-07-03 19:33:19 +0800

    fix config init

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import os
-
 from src.lib.data_process import process_data
 from src.lib.ui import create_ui
 from src.lib.folder_init import init_folder
-
+from src.common.util import AppConfig
 
 if __name__ == "__main__":
+    # 调用初始化函数
+    AppConfig.initialize()
 
     def on_loading():
         init_folder()
--- src/common/util.py
+++ src/common/util.py
@@ -2,26 +2,32 @@ import os
 import sys
 from src.common.config_process import ConfigProcess
 
-
 class AppConfig:
     origin_dir_key = 'origin_dir'
     pdf_suffix = '.pdf'
     txt_suffix = '.txt'
 
-    main_path = os.path.abspath(os.path.join(os.path.dirname(
-        os.path.abspath(__file__)), '..', '..'))
-    if getattr(sys, 'frozen', False):
-        main_path = os.path.abspath(os.path.dirname(sys.executable))
-    config_path = os.path.abspath(os.path.join(main_path, '.config'))
-    if not os.path.exists(config_path):
-        with open(config_path, "w") as f:
-                    f.write('')
-    _config_processor = ConfigProcess(config_path)
-    default_input_path = os.path.abspath(os.path.join(main_path, 'origin'))
-    output_path = os.path.abspath(os.path.join(main_path, 'source'))
-    data_path = os.path.abspath(os.path.join(main_path, 'data.json'))
+    main_path = None
+    config_path = None
+    _default_input_path = None
+    output_path = None
+    data_path = None
+
+    @classmethod
+    def initialize(cls):
+        cls.main_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
+        if getattr(sys, 'frozen', False):
+            cls.main_path = os.path.abspath(os.path.dirname(sys.executable))
+        cls.config_path = os.path.abspath(os.path.join(cls.main_path, '.config'))
+        if not os.path.exists(cls.config_path):
+            with open(cls.config_path, "w") as f:
+                f.write('')
+        cls._default_input_path = os.path.abspath(os.path.join(cls.main_path, 'origin'))
+        cls.output_path = os.path.abspath(os.path.join(cls.main_path, 'source'))
+        cls.data_path = os.path.abspath(os.path.join(cls.main_path, 'data.json'))
 
     @classmethod
     def get_input_path(cls):
-        return os.path.abspath(cls._config_processor.read_config(cls.origin_dir_key)
-                               if cls._config_processor.has_key(cls.origin_dir_key) else cls.default_input_path)
+        config_processor = ConfigProcess(cls.config_path)
+        return os.path.abspath(config_processor.read_config(cls.origin_dir_key)
+                               if config_processor.has_key(cls.origin_dir_key) else cls._default_input_path)
